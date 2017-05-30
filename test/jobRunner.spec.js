a564aeb840ca73459c6be8186160c2b5bbd2d312
DESKTOP-QU4A5Q9\Nelson <nelsonfamorim@gmail.com>
2017-05-30 01:35:35 +0100

    remove some verbosity

--- test/jobRunner.spec.js
+++ test/jobRunner.spec.js
@@ -4,172 +4,50 @@ const jobRunner = require('../services/jobRunner');
 const should = chai.should();
 
 describe('jobRunner tests', ()=>{
+
+    function jobBuilder(type,position,conf,done){
+        return {
+            type: type,
+            run: (callback)=>{
+                setTimeout(()=>{
+                    should.equal(conf.runPosition++,position);
+                    callback();
+                    if(done)
+                        done();
+                },100);
+            }
+        }
+    }
+
     it('check if follows basic order', (done)=>{
-        var runPosition = 1;
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,1);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,2);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,3);
-                    callback();
-                    done();
-                },100);
-            }
-        });
+        var conf = {runPosition: 1};
+        jobRunner.queueJob(jobBuilder('HTML',1,conf));
+        jobRunner.queueJob(jobBuilder('HTML',2,conf));
+        jobRunner.queueJob(jobBuilder('HTML',3,conf,done));
     });
 
     it('prioritize max html files', (done)=>{
-        var runPosition = 1;
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,1);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'PDF',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,7);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,2);
-                    callback();
-                },100);
-            }
-        });jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,3);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,4);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,5);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,6);
-                    callback();
-                    done();
-                },100);
-            }
-        });
+        var conf = {runPosition: 1};
+        jobRunner.queueJob(jobBuilder('HTML',1,conf));
+        jobRunner.queueJob(jobBuilder('PDF',7,conf, done));
+        jobRunner.queueJob(jobBuilder('HTML',2,conf));
+        jobRunner.queueJob(jobBuilder('HTML',3,conf));
+        jobRunner.queueJob(jobBuilder('HTML',4,conf));
+        jobRunner.queueJob(jobBuilder('HTML',5,conf));
+        jobRunner.queueJob(jobBuilder('HTML',6,conf));
     });
 
     
 
     it('prioritize until next PDF', (done)=>{
-        var runPosition = 1;
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,1);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'PDF',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,5);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,2);
-                    callback();
-                },100);
-            }
-        });jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,3);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,4);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'PDF',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,7);
-                    callback();
-                },100);
-            }
-        });
-        jobRunner.queueJob({
-            type: 'HTML',
-            run: (callback)=>{
-                setTimeout(()=>{
-                    should.equal(runPosition++,6);
-                    callback();
-                    done();
-                },100);
-            }
-        });
+        var conf = {runPosition: 1};
+        jobRunner.queueJob(jobBuilder('HTML',1,conf));
+        jobRunner.queueJob(jobBuilder('PDF',5,conf));
+        jobRunner.queueJob(jobBuilder('HTML',2,conf));
+        jobRunner.queueJob(jobBuilder('HTML',3,conf));
+        jobRunner.queueJob(jobBuilder('HTML',4,conf));
+        jobRunner.queueJob(jobBuilder('PDF',7,conf,done));
+        jobRunner.queueJob(jobBuilder('HTML',6,conf));
     });
 });
 
